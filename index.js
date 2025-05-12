2f390dad904f60cf4177fae77648f81f616e887b
shubham79a <shubh79.nolan@gmail.com>
2025-05-12 23:48:25 +0530

    final commit

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/test-db', async (req, res) => {
 });
 
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to the server");
+})
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
